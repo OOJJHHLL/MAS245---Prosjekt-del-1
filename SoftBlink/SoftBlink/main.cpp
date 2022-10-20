2c716de4177aa28d5b1b8a2a700271151d391719
Simon Storaker <simonst@student.uia.no>
2022-10-20 16:14:40 +0200

    Added SoftBlink

--- SoftBlink/SoftBlink/main.cpp
+++ SoftBlink/SoftBlink/main.cpp
@@ -5,25 +5,53 @@
  * Author : Simon Storaker
  */ 
 //#ifndef F_CPU
-#define F_CPU 1000000UL
+#define F_CPU 1000000UL // 1MHz
 //#endif
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 
+void blinkLED()
+{
+		/*DDRC = (1 << PC5); // Setter PIN som output (likt som pinMode i arduino)
+
+		while (1)
+		{
+			PORTC &= ~(1 << PC5); // Setter PIN 5 portC lav
+			_delay_ms(1000); // 1 sekund delay
+			
+			PORTC |= (1 << PC5); //Setter PIN 5 portC høy
+			_delay_ms(1000); // 1 sekund delay
+		}*/
+	
+}
+
+
+void setup()
+{
+	//DDRB = (1 << PB1); // Setter PIN som output (likt som pinMode i arduino)
+}
+
+void pwm (int dutycycle, int periode)
+{
+	
+}
+
 int main(void)
 {
 	
-	DDRC = (1 << PC5); // Setter PIN som output
-
+	int brightness = 0;
+	int fade = 5;
+	
+	DDRB = (1 << PB1);
 	while (1) 
     {		
-		PORTC &= ~(1 << PC5); 
-		_delay_ms(1000); //so that LED would remain in ON state for 1 sec and then  turns off  for  1 sec.
-		
-		PORTC |= (1 << PC5);
-		_delay_ms(1000);
+		PORTB &= ~(1 << PB1); // Setter PIN 5 portC lav
+		_delay_ms(1000); // 1 sekund delay
+
+		PORTB |= (1 << PB1); //Setter PIN 5 portC høy
+		_delay_ms(1000); // 1 sekund delay
     }
 }
 
